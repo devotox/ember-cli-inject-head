19feba99a378e0adcc215e83285760f42b6b63ea
Devonte <devo.tox.89@gmail.com>
2015-07-12 19:31:41 +0100

    wrap read file in try catch

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 /* jshint node: true */
 'use strict';
 
+var inject = false;
+
 var fs = require('fs');
 
 var Handlebars = require('handlebars');
 
-var inject = false;
-
 module.exports = {
   name: 'ember-cli-inject-head',
 
-  contentFor: function(type, config){   
-    if( inject && type === 'head' ) {
-      return inject;
-    }
+  contentFor: function(type, config){
+	if( inject && type === 'head' ) {
+	  return inject;
+	}
   },
 
   serverMiddleware: function(config) {
-    var options = config.options['inject-head'];
+	var options = config.options['inject-head'];
 
-    if( options && options.template ) {
-      var head = fs.readFileSync(options.template, 'utf8').toString();
-      return inject = Handlebars.compile(head)(options.context); 
-    }  
+	if( options && options.template ) {
+	  try {
+		var head = fs.readFileSync(options.template, 'utf8').toString();
+	  } catch (err) {
+		return inject = false;
+	  }
+	  return inject = Handlebars.compile(head)(options.context);
+	}
   }
 };
